d3e6a31836ee00348bf3facf82e97eea45f23e49
cihuuy <95659029+cihuuy@users.noreply.github.com>
2021-12-24 12:10:18 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,14 +1,3 @@
-checkroot() {
-
-if [[ "$(id -u)" -ne 0 ]]; then
-   printf "\e[1;77mPlease, run this program as root!\n\e[0m"
-   exit 1
-fi
-
-}
-
-checkroot
-
 sudo apt install neofetch -y
 clear
 echo "----------------------------------------------"
